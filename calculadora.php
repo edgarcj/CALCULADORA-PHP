e9db420ecfb3a995ff1b88377ecaf51fffcbde2a
edy cam <edgar_traseur_hiphop@hotmail.com>
2021-01-20 19:33:27 -0600

    Comienzo del proyecto

---
+++ calculadora.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style></style>
+</head>
+<body>
+  
+  <form action="calculadora.php" method="get">
+ <h1>CALCULADORA PHP</h1>
+    <input type="text" name="numero1" value="">
+    <input type="text" name="numero2" value="">
+    <input type="submit" name="" value="RESPUESTA">
+  
+  </form>
+
+</body>
+</html>
+<?php
+if(isset($_GET['numero1'])&& isset($_GET['numero2']) && is_numeric($_GET['numero1']) && is_numeric($_GET['numero2']) )//is_numeric identificar cada numero por lo que es
+{
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+    echo $numero1 + $numero2;
+}
+  
+
+
+
+?>
